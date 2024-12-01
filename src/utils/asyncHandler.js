270de110b8bd8706cfd95dab1a32a01817812c6e
Manoranjan1k <manasj790@gmail.com>
2024-12-01 16:49:03 +0530

    util code ApiErr and ApiRes

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,36 @@
+const asyncHandler = (reqestHandler) =>{
+    (req, res, next) => {
+        Promise.resolve(reqestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+
+
+
+
+export {asyncHandler}
+
+
+// const asyncHandler =()=>{}
+// const asyncHandler = (fucn) => ()=>{}
+// const asyncHandler = (func) => async () => {}    
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//         try {
+//             await fn(req, res, next)
+//         } catch (error) {
+//             res.status(error.code || 500).json({
+//                 success: false,
+//                 message: error.message
+//             })
+//         }
+//     }
+
+
+
+
+
+
+

1477c8104d4554da4e1239ff551fd3d87d6c426a
Manoranjan1k <manasj790@gmail.com>
2024-12-12 17:04:22 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import {uploadOnCloudinary} from "../utils/cloudnary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async (req, res) => {
     //  return res
     
     const {fullName, email, username, password} = req.body
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
 
     if(
@@ -24,18 +24,20 @@ const registerUser = asyncHandler(async (req, res) => {
     ){
         throw new ApiError(400, "All fields are required")
     } 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
-    console.log("existedUser: ", existedUser);
+    // console.log("existedUser: ", existedUser);
 
     if(existedUser){
         throw new ApiError(409, "User with email or username already exists")
     }   
     // console.log(req.files);
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    const coverImageLocalPath = req.files?.coverImage ? req.files?.coverImage[0]?.path : null
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
